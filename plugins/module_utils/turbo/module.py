d33d274492c1f5ef96fae4c54999f767bdffefc9
Gonéri Le Bouder <goneri@lebouder.net>
2020-10-23 15:42:30 -0400

    extend the unit-test coverage

--- plugins/module_utils/turbo/module.py
+++ plugins/module_utils/turbo/module.py
@@ -23,12 +23,55 @@ def collection_name():
         return ".".join(ansiblez[8:].split(".")[:2])
 
 
+def connect(socket_path):
+    running = False
+    _socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    for attempt in range(100, -1, -1):
+        try:
+            _socket.connect(socket_path)
+            return _socket
+        except (ConnectionRefusedError, FileNotFoundError):
+            if not running:
+                running = start_daemon(socket_path=socket_path)
+            if attempt == 0:
+                raise
+        time.sleep(0.01)
+
+
+def start_daemon(socket_path, ttl=None):
+    ansiblez_path = sys.path[0]
+    env = os.environ
+    env.update({"PYTHONPATH": ansiblez_path})
+    import subprocess
+
+    parameters = [
+        "--fork",
+        "--socket-path",
+        socket_path,
+    ]
+
+    if ttl:
+        parameters += ["--ttl", str(ttl)]
+
+    p = subprocess.Popen(
+        [
+            sys.executable,
+            "-m",
+            "ansible_collections.cloud.common.plugins.module_utils.turbo.server",
+        ]
+        + parameters,
+        env=env,
+        close_fds=True,
+    )
+    p.communicate()
+    return p.pid
+
+
 class AnsibleTurboModule(ansible.module_utils.basic.AnsibleModule):
     embedded_in_server = False
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._socket = None
         self._socket_path = (
             os.environ["HOME"] + f"/.ansible/tmp/turbo_mode.{collection_name()}.socket"
         )
@@ -37,45 +80,8 @@ class AnsibleTurboModule(ansible.module_utils.basic.AnsibleModule):
         if not self.embedded_in_server:
             self.run_on_daemon()
 
-    def start_daemon(self):
-        if self._running:
-            return
-
-        ansiblez_path = sys.path[0]
-        env = os.environ
-        env.update({"PYTHONPATH": ansiblez_path})
-        import subprocess
-
-        p = subprocess.Popen(
-            [
-                sys.executable,
-                "-m",
-                "ansible_collections.cloud.common.plugins.module_utils.turbo.server",
-                "--fork",
-                "--socket-path",
-                self._socket_path,
-            ],
-            env=env,
-            close_fds=True,
-        )
-        self._running = True
-        p.communicate()
-        return
-
-    def connect(self):
-        self._socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        for attempt in range(100, -1, -1):
-            try:
-                self._socket.connect(self._socket_path)
-                return
-            except (ConnectionRefusedError, FileNotFoundError):
-                self.start_daemon()
-                if attempt == 0:
-                    raise
-            time.sleep(0.01)
-
     def run_on_daemon(self):
-        self.connect()
+        _socket = connect(socket_path=self._socket_path)
         result = dict(changed=False, original_message="", message="")
         ansiblez_path = sys.path[0]
         args = {
@@ -87,14 +93,14 @@ class AnsibleTurboModule(ansible.module_utils.basic.AnsibleModule):
             ansiblez_path,
             json.dumps(args),
         ]
-        self._socket.send(json.dumps(data).encode())
+        _socket.send(json.dumps(data).encode())
         raw_answer = b""
         while True:
-            b = self._socket.recv((1024 * 10))
+            b = _socket.recv((1024 * 10))
             if not b:
                 break
             raw_answer += b
-        self._socket.close()
+        _socket.close()
 
         result = json.loads(raw_answer.decode())
         self.exit_json(**result)
--- tests/unit/module_utils/test_turbo_module.py
+++ tests/unit/module_utils/test_turbo_module.py
@@ -3,10 +3,17 @@
 # GNU General Public License v3.0+
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+# py38 only, See: https://github.com/PyCQA/pylint/issues/2976
+from unittest.mock import Mock, ANY  # pylint: disable=syntax-error
+import time
 import pytest
+import socket
+import subprocess
 import ansible.module_utils.basic
 from ansible_collections.cloud.common.plugins.module_utils.turbo.module import (
     collection_name,
+    connect,
+    start_daemon,
 )
 
 
@@ -25,3 +32,37 @@ def test_collection_name(monkeypatch, my_module_path, my_collection_name):
 
     monkeypatch.setattr(ansible.module_utils.basic, "get_module_path", mocked_func)
     assert collection_name() == my_collection_name
+
+
+def test_start_daemon(monkeypatch):
+    mocked_Popen = Mock()
+    monkeypatch.setattr(subprocess, "Popen", mocked_Popen)
+    assert start_daemon("/aa")
+    mocked_Popen.assert_called_once_with(
+        [
+            ANY,
+            "-m",
+            "ansible_collections.cloud.common.plugins.module_utils.turbo.server",
+            "--fork",
+            "--socket-path",
+            "/aa",
+        ],
+        env=ANY,
+        close_fds=True,
+    )
+
+
+def test_start_daemon_with_no_mock(tmp_path):
+    my_socket = tmp_path / "socket"
+    assert start_daemon(socket_path=str(my_socket), ttl=1)
+    time.sleep(0.5)
+    assert my_socket.is_socket()
+    time.sleep(0.8)
+    assert not my_socket.exists()
+
+
+def test_connect(monkeypatch):
+    mocked_socket = Mock()
+    monkeypatch.setattr(socket, "socket", mocked_socket)
+    assert connect("/nowhere")
+    mocked_socket.connect_assert_called_once_with("/nowhere")
